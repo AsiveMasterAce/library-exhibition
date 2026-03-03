c78400027a07fed608cb36f779522e77e15c92b4
Asive Nzwana <AsiveN@nkqubela.co.za>
2026-03-03 15:12:27 +0200

    register changes

--- pages/register.vue
+++ pages/register.vue
@@ -3,7 +3,17 @@
     <Navbar />
     <div class="min-h-screen text-accent">
       <div class="py-16">
-        <RegistrationForm />
+        <div class="max-w-4xl mx-auto px-4">
+          <h1 class="font-serif text-3xl md:text-4xl mb-6 text-center">Register to Attend</h1>
+          <iframe
+            :src="msFormUrl"
+            class="w-full h-[800px] bg-white rounded-xl shadow-2xl border-0"
+            frameborder="0"
+            marginwidth="0"
+            marginheight="0"
+            allowfullscreen
+          ></iframe>
+        </div>
       </div>
       <Footer />
     </div>
@@ -13,5 +23,7 @@
 <script setup lang="ts">
 import Navbar from '~/components/layout/Navbar.vue';
 import Footer from '~/components/layout/Footer.vue';
-import RegistrationForm from '~/components/form/RegistrationForm.vue';
+
+const msFormUrl =
+  'https://forms.office.com/pages/responsepage.aspx?id=5OLy_mythkSNqKI-HjYqC2Hk93RY0ZhClQo649zvC7pUOTMxTUZUR0dKQkhRNkZKS0ZQQ0dWTzlTRi4u&route=shorturl&embed=true';
 </script>
