4f3d6aef41aa8ea23a177a740a1b8120f3914c0e
Asive Nzwana <AsiveN@nkqubela.co.za>
2026-03-05 09:12:46 +0200

    button

--- components/layout/Navbar.vue
+++ components/layout/Navbar.vue
@@ -4,9 +4,16 @@
       <a href="/">
         <img src="/images/logos/dsrac.png" alt="Sports and Culture Logo" class="h-10 w-auto mr-4" />
       </a>
-
     </div>
 
+    <div class="flex items-center gap-4">
+      <NuxtLink
+        to="/register"
+        class="bg-[#77bc44]/30 text-[#345222] px-4 py-2 rounded-lg font-semibold  transition-all duration-300 hover:scale-[1.03] hover:shadow-lg"
+      >
+        Register
+      </NuxtLink>
+    </div>
   </nav>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,13 @@
   <div>
     <Navbar />
     <div class="min-h-screen text-accent container mx-auto">
-        <HeroSection />
-          <FuturisticPatternSection />
-        <AboutSection />
-        <ProgrammeSection />
-        <!-- <SpeakersSection /> -->
-        <!-- <ScheduleSection /> -->
-        <!-- <RegisterCTA /> -->
-        <GallerySection />
-        <PartnersSection />
-        <!-- <MediaResources /> -->
-        <ContactSection />
+      <HeroSection />
+      <FuturisticPatternSection />
+      <AboutSection />
+      <ProgrammeSection />
+      <GallerySection />
+      <PartnersSection />
+      <ContactSection />
       <Footer />
     </div>
   </div>
@@ -22,15 +18,10 @@
 import Navbar from '~/components/layout/Navbar.vue';
 import Footer from '~/components/layout/Footer.vue';
 import HeroSection from '~/components/exhibition/HeroSection.vue';
-import ScheduleSection from '~/components/exhibition/ScheduleSection.vue';
 import GallerySection from '~/components/exhibition/GallerySection.vue';
-import RegisterCTA from '~/components/exhibition/RegisterCTA.vue';
-  import SpeakersSection from '~/components/exhibition/SpeakersSection.vue';
-  import SponsorsSection from '~/components/exhibition/SponsorsSection.vue';
-  import AboutSection from '~/components/exhibition/AboutSection.vue';
-  import ProgrammeSection from '~/components/exhibition/ProgrammeSection.vue';
-  import PartnersSection from '~/components/exhibition/PartnersSection.vue';
-  import MediaResources from '~/components/exhibition/MediaResources.vue';
-  import ContactSection from '~/components/exhibition/ContactSection.vue';
-  import FuturisticPatternSection from '~/components/exhibition/FuturisticPatternSection.vue';
+import AboutSection from '~/components/exhibition/AboutSection.vue';
+import ProgrammeSection from '~/components/exhibition/ProgrammeSection.vue';
+import PartnersSection from '~/components/exhibition/PartnersSection.vue';
+import ContactSection from '~/components/exhibition/ContactSection.vue';
+import FuturisticPatternSection from '~/components/exhibition/FuturisticPatternSection.vue';
 </script>
